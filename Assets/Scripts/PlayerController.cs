540928a6f1acffe1f981208a652526b80ab87af5
Bojan <page0itv@gmail.com>
2020-08-19 11:26:28 +0200

    Turn Player to mouse cursor
    Moving and turning the player with rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,58 +10,78 @@ public class PlayerController : MonoBehaviour
 
     private float horizontal;
     private float vertical;
-    private float mouseHorizontal;
-    private float camRayLenght = 100f;
+    private float camRayLength = 100f;
     private int floorMask;
     private Vector3 movement;
+    private Rigidbody rigidbody;
+    // private Animator animator;
 
     private void Awake()
     {
         floorMask = LayerMask.GetMask("Floor");
+        rigidbody = GetComponent<Rigidbody>();
+        // animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-        mouseHorizontal = Input.GetAxis("Mouse X");
 
-        // Move faster if player is running
-        if (Input.GetKey(KeyCode.LeftShift) && isMoving())
-        {
-            movement.x = horizontal;
-            movement.z = vertical;
-
-            transform.Translate(Vector3.ClampMagnitude(movement, 1f) * (speed * 2) * Time.deltaTime);
-        }
-        // Move normal speed in scene
-        else
-        {
-            movement.x = horizontal;
-            movement.z = vertical;
-
-            transform.Translate(Vector3.ClampMagnitude(movement, 1f) * speed * Time.deltaTime);
-        }
-
-        // Turning the palyer in Scene
-        transform.Rotate(Vector3.up, mouseHorizontal * mouseSensitivity * Time.deltaTime);
     }
 
-    // Check if palyer is inputing
-    private bool isMoving()
+    private void FixedUpdate()
     {
-        if (horizontal > 0.1 || horizontal < -0.1 || vertical > 0.1 || vertical < -0.1)
+        // Store the input axes.
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Move the player around the scene.
+        Move(horizontal, vertical);
+
+        // Turn the player to face the mouse cursor.
+        Turning();
+    }
+
+    void Move(float horizontal, float vertical)
+    {
+        // Set the movement vector based on the axis input.
+        movement.Set(horizontal, 0f, vertical);
+
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        rigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        // Create a ray from the mouse cursor on screen in the direction of the camera.
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Create a RaycastHit variable to store information about what was hit by the ray.
+        RaycastHit floorHit;
+
+        // Perform the raycast and if it hits something on the floor layer...
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
         {
-            return true;
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            // Ensure the vector is entirely along the floor plane.
+            playerToMouse.y = 0f;
+
+            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            // Set the player's rotation to this new rotation.
+            rigidbody.MoveRotation(newRotation);
         }
-        else
-            return false;
     }
 }
