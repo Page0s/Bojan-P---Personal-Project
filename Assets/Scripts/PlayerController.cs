5e24d66e35776e53a270efbd2e88cf628f62892a
Bojan <page0itv@gmail.com>
2020-09-04 14:20:37 +0200

    Finish Player Controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,12 +33,6 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void FixedUpdate()
     {
         // Store the input axes.
