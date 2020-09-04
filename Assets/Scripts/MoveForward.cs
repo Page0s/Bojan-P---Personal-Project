4f78cb9b8e9ac0fa04ae6effffce1d127c3349fb
Bojan <page0itv@gmail.com>
2020-09-04 15:52:06 +0200

    Add MoveForward on Enemys
    Moving forward direction

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveForward : MonoBehaviour
+{
+    [SerializeField] private float forceAmount = 5f;
+
+    private Rigidbody rigidbody;
+    private Vector3 newMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position + (transform.forward * Time.deltaTime * forceAmount));
+    }
+}
