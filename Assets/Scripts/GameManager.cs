9e43ffdba228bec67996c1ac59c9bd2e6976f091
Bojan <page0itv@gmail.com>
2020-09-04 15:50:47 +0200

    Add GameManager and enemy spawner
    Spawning 5 enemys infront of player

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private Transform spawnLocation;
+
+    private float enemyAmount = 5;
+    private bool endGame = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyWave());
+    }
+
+    private IEnumerator SpawnEnemyWave()
+    {
+        for(int i = 0; i < enemyAmount; ++i)
+        {
+            spawnEnemy();
+            yield return new WaitForSeconds(2);
+        }
+        endGame = true;
+    }
+
+    private void spawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnLocation.position, spawnLocation.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (endGame)
+        {
+            StopCoroutine(SpawnEnemyWave());
+        }
+    }
+}
