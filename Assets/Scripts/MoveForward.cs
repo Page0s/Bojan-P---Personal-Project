9451ff12894665690890e6781035edc81ff8e999
Bojan <page0itv@gmail.com>
2020-09-04 18:04:20 +0200

    Add enemy move and rotate to player location

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,18 +8,33 @@ public class MoveForward : MonoBehaviour
 {
     [SerializeField] private float forceAmount = 5f;
 
+    private Transform playerTransform;
     private Rigidbody rigidbody;
-    private Vector3 newMovement;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();    
+        rigidbody = GetComponent<Rigidbody>();
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rigidbody.MovePosition(transform.position + (transform.forward * Time.deltaTime * forceAmount));
+        // Move enemy to player position
+        rigidbody.MovePosition(transform.position + GetDirection().normalized * forceAmount * Time.deltaTime);
+
+        // Rotate to player position
+        rigidbody.MoveRotation(GetDeltaRotation());
+    }
+
+    private Quaternion GetDeltaRotation()
+    {
+        return Quaternion.LookRotation(GetDirection());
+    }
+
+    private Vector3 GetDirection()
+    {
+        return playerTransform.position - transform.position;
     }
 }
