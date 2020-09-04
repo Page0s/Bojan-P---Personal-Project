84170bd39e4c7ddc9b9de4cc1315d716c812e238
Bojan <page0itv@gmail.com>
2020-09-04 19:31:10 +0200

    Add PlayerStats.cs
    Add Damage() and IsDead() methods

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+    [SerializeField] private int stamina = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int damage)
+    {
+        if(health > 0)
+        {
+            health -= damage;
+        }
+    }
+
+    public bool isDead() => health > 0;
+}
