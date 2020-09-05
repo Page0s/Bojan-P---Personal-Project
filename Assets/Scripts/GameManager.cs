f1c740a9a31f606ffcdd6f32405cf6e2cc4eb061
Bojan <page0itv@gmail.com>
2020-09-05 15:27:19 +0200

    Add damage logic for enemy and player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,10 +10,12 @@ public class GameManager : MonoBehaviour
 
     private float enemyAmount = 5;
     private bool endGame = false;
+    private PlayerStats playerStats;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerStats = GameObject.Find("Player").GetComponent<PlayerStats>();
         StartCoroutine(SpawnEnemyWave());
     }
 
@@ -36,9 +38,41 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Stop the game when endGame == true
         if (endGame)
         {
+            Debug.Log("Stop spawning enemys!");
             StopCoroutine(SpawnEnemyWave());
         }
+
+        if (playerStats.isDead())
+        {
+            endGame = true;
+            Debug.Log("Game Over!");
+        }
+    }
+
+    // Damage enemy's HP by projectile damage amount
+    public void DamageEnemy(GameObject other, int damage)
+    {
+        other.gameObject.GetComponent<EnemyStats>().DamageEnemy(damage);
+
+        Debug.Log(other.gameObject.GetComponent<EnemyStats>().Health);
+
+        // Enemy dies if HP is 0
+        if (other.gameObject.GetComponent<EnemyStats>().isDead())
+        {
+            Destroy(other.gameObject);
+
+            Debug.Log("Enemy died!");
+        }
+    }
+
+    // Damage player's HP by projectile damage amount
+    internal void DamagePlayer(Collision collision, int health)
+    {
+        playerStats.Damage(collision.gameObject.GetComponent<EnemyStats>().Damage);
+
+        Debug.Log(playerStats.Health);
     }
 }
