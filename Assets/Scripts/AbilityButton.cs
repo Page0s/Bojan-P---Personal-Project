7eb3976898bf35c48f14ad1e10e8e351371321f8
Bojan <page0itv@gmail.com>
2020-09-05 20:19:20 +0200

    Add AbilityButton.cs
    Improves character and weapon stats

---
+++ Assets/Scripts/AbilityButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityButton : MonoBehaviour
+{
+    [SerializeField] private int abilityIndex = 1;
+    private GameManager gameManager;
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        //button = GetComponent<Button>();
+        //button.onClick.AddListener(AbilityModifier);
+    }
+
+    private void AbilityModifier()
+    {
+        //Debug.Log($"{button.gameObject.name} was clicked!");
+        gameManager.ModifyAbility(abilityIndex);
+    }
+}
