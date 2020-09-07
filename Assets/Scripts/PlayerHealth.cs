2b263e44c0175935d5c564fe0fffd374a76c4a90
Bojan <page0itv@gmail.com>
2020-09-07 17:18:42 +0200

    Modify PlayerHealth update HP Bar UI

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,34 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     private Slider slider;
+    private TMP_Text healthText;
     private float maxHealth = 100;
     private float currentHealth;
+    private string originText = "HP :";
 
     private void Awake()
     {
         slider = GetComponent<Slider>();
+        healthText = GameObject.Find("HealthText").GetComponent<TMP_Text>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthText.text = originText + currentHealth.ToString();
         slider.value = CalculateHealth();
     }
 
     private float CalculateHealth()
     {
-        //return currentHealth / maxHealth;
         return currentHealth;
     }
 
     public void UpdateHealth(float amount)
     {
         currentHealth = amount;
+        healthText.text = originText + currentHealth.ToString();
         slider.value = CalculateHealth();
     }
 }
