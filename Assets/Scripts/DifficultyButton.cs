24489a9bea71a689ee9c07a354b6626b9c8cc397
Bojan <page0itv@gmail.com>
2020-09-07 20:58:30 +0200

    Add DifficultyButton.cs

---
+++ Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    [SerializeField] private int difficultyIndex;
+
+    private Button button;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    private void SetDifficulty()
+    {
+        Debug.Log($"{button.gameObject.name} button was clicked");
+        gameManager.StartGame(difficultyIndex);
+
+        Button[] buttons = FindObjectsOfType<Button>();
+
+        foreach (Button button in buttons)
+            button.gameObject.SetActive(false);
+    }
+}
